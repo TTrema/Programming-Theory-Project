9a86b61ff3c682fac989824ed132ac8f91bed627
jwill <114110689+TTrema@users.noreply.github.com>
2023-05-20 13:49:18 -0300

    title screen created

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }   
+
+    public void Exit()
+        {
+            #if UNITY_EDITOR
+                    EditorApplication.ExitPlaymode();
+            #else
+                    Application.Quit(); // original code to quit Unity player
+            #endif
+        }
+
+}
